964ac2d086b9667cbc221926674fb2d1cb337dee
julien colomb <julien.colomb@fu-berlin.de>
2020-09-17 20:07:22 +0200

    update and expand code

--- createwebsite.r
+++ createwebsite.r
@@ -1,24 +1,38 @@
 #setting up website
+
+## install dependencies in terminal:
+# brew install git golang hugo
+
+
 ## get packages
-packrat::restore()
+renv::restore()
 
 library(blogdown)
 
 ## install academic website
 dir.create("website")
 blogdown::install_hugo(force=TRUE) # to make sure the latest version of hugo is used
-blogdown::new_site(dir = "website",theme="gcushen/hugo-academic")
+
+blogdown::new_site(dir = "website",theme="wowchemy/starter-academic")
+
+
 
 # note to get to an older hugo version, for instance 0.58.3:
 #if (blogdown::hugo_version() != "0.58.3") blogdown::install_hugo(version ="0.58.3", force =TRUE, use_brew = FALSE)
 
 # moving and modifying folders
 
-file.rename("website/content/home", "trash")
+file.rename("website/content/home", "trash/website")
 file.rename("home", "website/content/home")
+#file.rename("website/layouts", "trash/layout")
 file.rename("layouts", "website/layouts")
+file.rename("website/content/project", "trash/project")
+dir.create("website/content/project")
 
-
+blogdown::stop_server()
+setwd("website")
+blogdown::build_site()
 blogdown::serve_site()
+setwd("../")
 
 #file.rename( "trash", "website/content/home", recursive=TRUE)
